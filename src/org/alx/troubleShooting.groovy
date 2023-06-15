046ec01e530d066f0dbd5de7b94b1e9c3a0fd16a
Alexander Bazhenov <bazhenoff.alexander@gmail.com>
2023-06-15 19:13:16 +0300

    wip

--- src/org/alx/troubleShooting.groovy
+++ src/org/alx/troubleShooting.groovy
@@ -3,9 +3,21 @@ package org.alx
 
 // https://stackoverflow.com/questions/54125894/jenkins-shared-library-src-class-unable-to-resolve-class
 class OrgAlxGlob implements Serializable {
-    public static String GitCredentialsID = ''
+    public String GitCredentialsID = '123'
+
+    OrgAlxGlob(String pName) {
+        this.GitCredentialsID = pName
+    }
+
+    def sayHi() {
+        return String.format("Hello, %s which is %s.", this.GitCredentialsID, this.GitCredentialsID.getClass())
+    }
+
+    def sayHi(String name) {
+        return String.format("Hello, %s which is %s.", name, name.getClass())
+    }
 }
 
 static TestFunctionInSrc(String cred = OrgAlxGlob.GitCredentialsID) {
-    return [String.format('src: %s', cred), cred.getClass(), OrgAlxGlob.GitCredentialsID.getClass()]
+    return [String.format('src: %s', cred), OrgAlxGlob.getClass(), OrgAlxGlob.sayHi]
 }
