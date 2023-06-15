ce9cfcc0c16abdbe343d7f0a5238e215cce43517
Alexander Bazhenov <bazhenoff.alexander@gmail.com>
2023-06-15 19:31:52 +0300

    wip

--- src/org/alx/troubleShooting.groovy
+++ src/org/alx/troubleShooting.groovy
@@ -4,13 +4,8 @@ package org.alx
 // https://stackoverflow.com/questions/54125894/jenkins-shared-library-src-class-unable-to-resolve-class
 class OrgAlxGlob {
     public static String GitCredentialsID = '123'
-
-    static sayHi() {
-        return String.format("Hello, %s which is %s.", GitCredentialsID, GitCredentialsID.getClass())
-    }
-
 }
 
 static TestFunctionInSrc(String cred = OrgAlxGlob.GitCredentialsID) {
-    return [String.format('src: %s', cred), OrgAlxGlob.sayHi().getClass(), OrgAlxGlob.sayHi()]
+    return [String.format('src: %s', cred)]
 }
