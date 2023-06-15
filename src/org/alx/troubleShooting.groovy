99a7ad714077e81492a781c495726683702f00a7
Alexander Bazhenov <bazhenoff.alexander@gmail.com>
2023-06-15 20:03:59 +0300

    wip

--- src/org/alx/troubleShooting.groovy
+++ src/org/alx/troubleShooting.groovy
@@ -6,6 +6,12 @@ class OrgAlxGlob {
     public static String GitCredentialsID = '123'
 }
 
+final String GitCredentialsID2 = '456'
+
 static TestFunctionInSrc(String cred = OrgAlxGlob.GitCredentialsID) {
+    return [String.format('src: %s', cred)]
+}
+
+static TestFunctionInSrc2(String cred = GitCredentialsID2) {
     return [String.format('src: %s', cred)]
 }
