52743cdbf6dda04da97a7b972b759569f20924e7
alexanderbazhenoff <bazhenoff.alexander@gmail.com>
2023-06-21 00:24:25 +0300

    runAnsible() update

--- src/org/alx/commonFunctions.groovy
+++ src/org/alx/commonFunctions.groovy
@@ -689,9 +689,9 @@ Boolean installAnsibleGalaxyCollections(String ansibleGitUrl, String ansibleGitB
  *
  * @param ansiblePlaybookText - text content of ansible playbook/role.
  * @param ansibleInventoryText - text content of ansible inventory file.
- * @param ansibleGitUrl - Git URL of ansible project to clone and run. Leave empty for ansible collection mode,
- *                           but skip collection cloning and install.
- * @param ansibleGitBranch - gitlab branch of ansible project.
+ * @param ansibleGitUrl - (optional) Git URL of ansible project to clone and run. Leave empty for ansible collection
+ *                        mode, but skip collection cloning and install.
+ * @param ansibleGitBranch - (optional) gitlab branch of ansible project.
  * @param ansibleExtras - (optional) extra params for playbook running.
  * @param ansibleCollections - (optional) list of ansible-galaxy collections dependencies which will be installed before
  *                             running the script. Collections should be placed in ansible gitlab project according to
@@ -704,8 +704,8 @@ Boolean installAnsibleGalaxyCollections(String ansibleGitUrl, String ansibleGitB
  * @param gitCredentialsId - Git credentialsID to clone ansible project.
  * @return - success (true when ok).
  */
-Boolean runAnsible(String ansiblePlaybookText, String ansibleInventoryText, String ansibleGitUrl,
-                   String ansibleGitBranch, String ansibleExtras = '', List ansibleCollections = [],
+Boolean runAnsible(String ansiblePlaybookText, String ansibleInventoryText, String ansibleGitUrl = '',
+                   String ansibleGitBranch = 'main', String ansibleExtras = '', List ansibleCollections = [],
                    String ansibleInstallation = '', Boolean cleanupBeforeAnsibleClone = true,
                    String gitCredentialsId = OrgAlxGlobals.GitCredentialsID) {
     Boolean runAnsibleState = false
